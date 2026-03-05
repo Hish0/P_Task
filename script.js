ada8f419aeaa5fa3cbd45751a8b9de9daab8e867
Hish0 <96497550+Hish0@users.noreply.github.com>
2026-03-05 15:14:58 +0100

    outline of what should the functions do

---
+++ script.js
@@ -0,0 +1,13 @@
+const input = document.getElementById("urlInput");
+const statusEl = document.getElementById("status");
+
+// simple URL regex validation, this is for the validation of the url before we push it to the server
+function isValidUrl(url) {}
+
+// mock async server, this is the server that will validate the url we will assume the file or folder exists
+function checkUrl(url) {}
+
+// this is the event listener for the input field , we will simulate the server call here and keep in mind the too many requests,we will be updating the status here
+input.addEventListener("input", () => {
+  const url = input.value;
+});
