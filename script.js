b4ed17dcc57670b327f084e0f784b07c0d0bfdfb
Hish0 <96497550+Hish0@users.noreply.github.com>
2026-03-05 15:25:27 +0100

    if the url in the server ends with / then it is a folder, otherwise it is a file

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ function isValidUrl(url) {
 }
 
 // mock async server, this is the server that will validate the url we will assume the file or folder exists
-function checkUrl(url) {}
+function checkUrl(url) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const type = url.endsWith("/") ? "folder" : "file";
+      resolve({ exists: true, type });
+    }, 500);
+  });
+}
 
 // this is the event listener for the input field , we will simulate the server call here and keep in mind the too many requests,we will be updating the status here
 input.addEventListener("input", () => {
